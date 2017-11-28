4c0d74f5f309cbe7e1c2dc1d68234249a12e4175
pagood <217xiaoyu@gmail.com>
2017-11-28 17:00:14 +0800

    set up WX config

--- app/controllers/api/v1/replies_controller.rb
+++ app/controllers/api/v1/replies_controller.rb
@@ -2,7 +2,22 @@ module Api
 	module V1
 		class RepliesController < Api::V1::ApplicationController
 			def index
-				render json: {msg: 'hello'}
+				begin
+					signature = params[:signature]
+					timestamp = params[:timestamp]
+					nonce = params[:nonce]
+					echostr = params[:echostr]
+					#no safe,need to save as env variable
+					token = Rails.application.secrets.WX_TOKEN
+					hashcode = Digest::SHA1.hexdigest [token, timestamp, nonce].sort.join
+					if hashcode == signature
+						render plain: echostr and return 
+					else
+						render plain: 'params errors' and return 
+					end
+				rescue 
+					render plain: 'server errors' and return
+				end
 			end
 		end
 	end 
